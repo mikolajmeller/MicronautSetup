12b87b9f72d00b9b777c75e1fd4927457a7f6a8b
mikolajmeller <meller.mikolaj.95@gmail.com>
2020-04-04 12:02:25 +0200

    setup project

---
+++ README.md
@@ -0,0 +1,6 @@
+## Testing project for micronaut integrations
+
+## TODO
+ * 
+### Sources
+ * Micronaut - https://docs.micronaut.io/1.3.3/guide/index.html
---
+++ src/main/java/micronautsetup/Application.java
@@ -0,0 +1,12 @@
+package micronautsetup;
+
+import io.micronaut.runtime.Micronaut;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+	public static void main(String[] args) {
+		Micronaut.run(Application.class);
+	}
+}
