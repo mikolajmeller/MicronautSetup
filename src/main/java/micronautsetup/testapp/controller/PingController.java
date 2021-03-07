c2dc4e2113c2e5e358585e5d62cad3cf1390df83
Mim <meller.mikolaj.95@gmail.com>
2021-03-07 12:03:58 +0100

    Use continous build and reload, bump version

--- src/main/java/micronautsetup/testapp/controller/PingController.java
+++ src/main/java/micronautsetup/testapp/controller/PingController.java
@@ -1,13 +1,16 @@
 package micronautsetup.testapp.controller;
 
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 
 @Controller
+@Produces(MediaType.APPLICATION_JSON)
 public class PingController {
     @Get
     public HttpResponse<String> ping() {
-        return HttpResponse.ok("ping");
+        return HttpResponse.ok("{\"value\": \"Ping pong\"}");
     }
 }
